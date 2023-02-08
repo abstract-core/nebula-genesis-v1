b775d9c277f7287970cb728cc55e49c10518e0a0
rimarok <romaric.ruga@mailo.fr>
2023-02-08 09:19:47 +0100

    cacheBlocksImage create a resized_image block

--- cacheBlocksImages/cacheBlocksImages.ts
+++ cacheBlocksImages/cacheBlocksImages.ts
@@ -1,8 +1,8 @@
-import { BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import { ExtendedBlockObjectResponse, ResizedImageBlockObject } from "statikon";
 import cacheImage from "./cacheImage/cacheImage";
 
 export default async function cacheBlocksImages(
-  blocks: BlockObjectResponse[],
+  blocks: ExtendedBlockObjectResponse[],
   siteUrl: string
 ) {
   await Promise.all(
@@ -11,9 +11,17 @@ export default async function cacheBlocksImages(
       .map(async (block) => {
         /* Redudant block type checking for TS typing */
         if (block.type === "image" && block.image.type === "file") {
+          /**
+           * @todo `cacheImage` should send back multiple urls
+           *  to provide `ResizedImageBlockObject` below.
+           */
           const filename = await cacheImage(block.image.file.url, siteUrl);
           if (filename) {
-            block.image.file.url = `/static/medias/${filename}`;
+            block = {
+              id: block.id,
+              type: "resized_image",
+              minUrl: `/static/medias/${filename}`,
+            } as ResizedImageBlockObject;
           }
         }
       })
